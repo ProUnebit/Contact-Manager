7b680a8b996f683f081801cd60eb3c2153e9bd2a
ProUnebit <ProUnebit@yandex.ru>
2018-09-26 00:36:12 +0300

    comp About.js is completed, remove build folder, version: 0.8

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import { theme } from '../../Styles/Theme'
-import { Paper, Grid, Typography, Button, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import { Paper, Grid, Typography, List, ListItem, ListItemText, Avatar } from '@material-ui/core'
 import { withStyles, MuiThemeProvider } from '@material-ui/core/styles'
-import { Drafts } from '@material-ui/icons'
 
 const styles = theme => ({
     gridContainer: {
         display: 'flex',
         justifyContent: 'center',
-        marginTop: '40px'
+        marginTop: 40
     },
     paper: {
         padding: theme.spacing.unit * 2.5,
-    },
-    list: {
-
-    },
+    }
 })
 
 const About =  (props) => {
@@ -30,43 +26,51 @@ const About =  (props) => {
                 className = {classes.gridContainer}>
                 <Grid item xs={10} sm={7} md={6} lg={5} xl={5}>
                     <Paper className = {classes.paper}>
-                        <Typography align="center">
-                            <h2>About Contact Manager</h2>
-                            <p>React.js App to manage contacts</p>
-                            <p>Version: <span style = {{color: '#FF7043'}}>0.8 Alfa</span></p>
+                        <Typography align="center" variant="title">
+                            About Contact Manager
                         </Typography>
-                        <Typography align="center">
-                            <h3>Follow me:</h3>
+                        <Typography align="center" variant="subheading" color="textSecondary">
+                            React.js App to manage contacts
+                        </Typography>
+                        <Typography align="center" variant="caption">
+                            Version: <span style = {{color: '#FF7043'}}>0.8 Alfa</span>
+                        </Typography>
+                        <Typography align="center" variant='title' style = {{marginTop: 30}}>
+                            Follow me:
                         </Typography>
                         <Grid
                             container
                             spacing={24}
-                            style = {{marginTop: '20px'}}>
+                            style = {{marginTop: 10, padding: '0 80px'}}>
                             <Grid item xs>
-                                <List className = {classes.list}>
-                                    <ListItem
-                                        button
-                                        onClick = {() => window.open('https://twitter.com/ProUnebit')}>
-                                        <ListItemText secondary="Twitter" />
-                                    </ListItem>
+                                <List>
                                     <ListItem
                                         button
                                         onClick = {() => window.open('https://github.com/ProUnebit')}>
-                                        <ListItemText secondary="GitHub" />
+                                        <Avatar style = {{backgroundColor: '#616161'}}>G</Avatar>
+                                        <ListItemText secondary="GitHub" align="center" />
+                                    </ListItem>
+                                    <ListItem
+                                        button
+                                        onClick = {() => window.open('https://twitter.com/ProUnebit')}>
+                                        <Avatar style = {{backgroundColor: '#2196F3'}}>T</Avatar>
+                                        <ListItemText secondary="Twitter" align="center" />
                                     </ListItem>
                                 </List>
                             </Grid>
                             <Grid item xs>
-                                <List className = {classes.list}>
+                                <List>
                                     <ListItem
                                         button
                                         onClick = {() => window.open('https://moikrug.ru/pro-une-bit')}>
-                                        <ListItemText secondary="Moikrug" />
+                                        <Avatar style = {{backgroundColor: '#FF5252'}}>M</Avatar>
+                                        <ListItemText secondary="Moikrug" align="center" />
                                     </ListItem>
                                     <ListItem
                                         button
                                         onClick = {() => window.open('https://www.linkedin.com/in/alexey-ratnikov')}>
-                                        <ListItemText  secondary="Linkedin" />
+                                        <Avatar style = {{backgroundColor: '#303F9F'}}>L</Avatar>
+                                        <ListItemText  secondary="Linkedin" align="center" />
                                     </ListItem>
                                 </List>
                             </Grid>
