9fbedbfd2095175602d78efe6c2815389b8ed092
ProUnebit <ProUnebit@yandex.ru>
2018-09-26 05:24:21 +0300

    test preloading app loader

--- src/App.js
+++ src/App.js
@@ -9,28 +9,44 @@ import { Provider } from './context'
 import SimpleBar from 'simplebar-react';
 import 'simplebar/dist/simplebar.min.css';
 import './Styles/index.css';
+import { LinearProgress } from '@material-ui/core';
 
 class App extends Component {
+
+    state = {
+        loading: true
+    }
+
+    componentDidMount() {
+        this.setState({
+            loading: false
+        });
+    }
+
   render() {
 
     return (
         <Provider>
             <Router>
-                <div>
-                    <Header branding="Contact Manager"/>
-                    <SimpleBar style = {{height: 'calc(100vh - 50px)'}}>
-                        <Switch>
-                            <Route exact path="/" component = {Contacts}/>
-                            <Route exact path="/contact/add" component = {AddContact}/>
-                            <Route exact path="/about" component = {About}/>
-                            <Route component = {NotFound}/>
-                        </Switch>
-                    </SimpleBar>
-                </div>
+                {this.state.loading ?
+                    <LinearProgress color="secondary" style = {{marginTop: 15, backgroundColor: '#64FFDA'}}/>
+                    :
+                    <div>
+                        <Header branding="Contact Manager"/>
+                        <SimpleBar style = {{height: 'calc(100vh - 50px)'}}>
+                            <Switch>
+                                <Route exact path="/" component = {Contacts}/>
+                                <Route exact path="/contact/add" component = {AddContact}/>
+                                <Route exact path="/about" component = {About}/>
+                                <Route component = {NotFound}/>
+                            </Switch>
+                        </SimpleBar>
+                    </div>
+                }
             </Router>
         </Provider>
-    );
-  }
+        )
+    }
 }
 
 export default App;
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,39 +1,52 @@
 import React from 'react'
 import Contact from './Contact'
 import { Consumer } from '../../context'
-import Typography from '@material-ui/core/Typography';
+import { Typography } from '@material-ui/core';
 
 class Contacts extends React.Component {
 
+    // state = {
+    //     loading: true
+    // }
+
+    componentDidMount() {
+        // setTimeout(() => {
+        //     this.setState({
+        //         loading: false
+        //     });
+        // }, 2000)
+    }
+
     render () {
 
-        return (
-            <Consumer>
-                {value => (
-                    <React.Fragment>
-                            <Typography
-                                variant="display1"
-                                align="center"
-                                style = {{textTransform: 'uppercase', marginTop: '40px'}}>
-                                <span style = {{color: '#FF7043', marginRight: '12px'}}>
-                                    Contact
-                                </span>
-                                    List
-                            </Typography>
-                        {value.contacts.map(contact => {
-                            return (
-                                <Contact
-                                    marginTop = { contact === value.contacts[0] ? {marginTop: '30px'} : null }
-                                    key = {contact.id}
-                                    contact = {contact}
-                                    />
-                            )
-                        })}
-                    </React.Fragment>
-                    )
-                }
-            </Consumer>
-        )
+        let contactList = value => (
+                <React.Fragment>
+                        <Typography
+                            variant="display1"
+                            align="center"
+                            style = {{textTransform: 'uppercase', marginTop: '40px'}}>
+                            <span style = {{color: '#FF7043', marginRight: '12px'}}>
+                                Contact
+                            </span>
+                                List
+                        </Typography>
+                    {value.contacts.map(contact => {
+                        return (
+                            <Contact
+                                marginTop = { contact === value.contacts[0] ? {marginTop: '30px'} : null }
+                                key = {contact.id}
+                                contact = {contact}
+                                />
+                        )
+                    })}
+                </React.Fragment>
+                )
+
+        // if (this.state.loading) {
+        //     contactList = value => <LinearProgress color="secondary" style = {{marginTop: 5, backgroundColor: '#64FFDA'}}/>
+        // }
+
+        return <Consumer>{contactList}</Consumer>
     }
 }
 
